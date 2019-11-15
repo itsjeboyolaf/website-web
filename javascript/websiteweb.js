9f5c02e74bf4dd73eac7feec76c0e8c8da6282cd
Olaf bon <45202699+itsjeboyolaf@users.noreply.github.com>
2019-11-15 01:17:48 +0100

    mobile detail

--- javascript/websiteweb.js
+++ javascript/websiteweb.js
@@ -1,9 +1,9 @@
 
-var slider = document.getElementById("leestijd")
-var output = document.getElementById("value")
-console.log(slider);
+var slider = document.getElementById("leestijd");
+var output = document.getElementById("value");
 
 output.innerHTML = slider.value; 
 slider.oninput = function() {
   output.innerHTML = this.value + " minuten";
 }
+
