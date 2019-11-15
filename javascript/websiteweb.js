d414b86e5572f6dcabd3cdf84abe99ceb5b107f0
Olaf bon <45202699+itsjeboyolaf@users.noreply.github.com>
2019-11-15 04:48:12 +0100

    it is done <3

--- javascript/websiteweb.js
+++ javascript/websiteweb.js
@@ -2,14 +2,27 @@
 var slider = document.getElementById("leestijd");
 var output = document.getElementById("value");
 var element = document.getElementById("dropdown")
-
+var toggle = false
 output.innerHTML = slider.value; 
 slider.oninput = function() {
   output.innerHTML = this.value + " minuten";
 }
 function dropdown(){
+    
+    if (window.innerWidth >800 && toggle == false) {
+       toggle = true;
+       element.classList.toggle("dropdown");
+    }
+    
+    else if(window.innerWidth >800 && toggle == true){
+        return;
+    }
+    else{
+    toggle = true;
     element.classList.toggle("dropdown");
+    }
 }
 
 document.getElementById("dropdown").addEventListener("click", dropdown);
+
 
