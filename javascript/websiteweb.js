8f806c93ae913292edf69ed9229b93259b96805f
Olaf bon <45202699+itsjeboyolaf@users.noreply.github.com>
2019-11-15 02:55:47 +0100

    op content na af ?!?!

--- javascript/websiteweb.js
+++ javascript/websiteweb.js
@@ -1,9 +1,15 @@
 
 var slider = document.getElementById("leestijd");
 var output = document.getElementById("value");
+var element = document.getElementById("dropdown")
 
 output.innerHTML = slider.value; 
 slider.oninput = function() {
   output.innerHTML = this.value + " minuten";
 }
+function dropdown(){
+    element.classList.toggle("dropdown");
+}
+
+document.getElementById("dropdown").addEventListener("click", dropdown);
 
