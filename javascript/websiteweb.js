2a583bb1c13e32c007e9be7a86940c7550612807
Olaf bon <45202699+itsjeboyolaf@users.noreply.github.com>
2019-11-15 14:27:15 +0100

    beautify

--- javascript/websiteweb.js
+++ javascript/websiteweb.js
@@ -1,28 +1,24 @@
-
 var slider = document.getElementById("leestijd");
 var output = document.getElementById("value");
-var element = document.getElementById("dropdown")
-var toggle = false
-output.innerHTML = slider.value; 
-slider.oninput = function() {
-  output.innerHTML = this.value + " minuten";
+var element = document.getElementById("dropdown");
+var toggle = false;
+
+output.innerHTML = slider.value;
+slider.oninput = function () {
+    output.innerHTML = this.value + " minuten";
 }
-function dropdown(){
-    
-    if (window.innerWidth >800 && toggle == false) {
-       toggle = true;
-       element.classList.toggle("dropdown");
-    }
-    
-    else if(window.innerWidth >800 && toggle == true){
+
+function dropdown() {
+
+    if (window.innerWidth > 800 && toggle == false) {
+        toggle = true;
+        element.classList.toggle("dropdown");
+    } else if (window.innerWidth > 800 && toggle == true) {
         return;
-    }
-    else{
-    toggle = true;
-    element.classList.toggle("dropdown");
+    } else {
+        toggle = true;
+        element.classList.toggle("dropdown");
     }
 }
 
 document.getElementById("dropdown").addEventListener("click", dropdown);
-
-
